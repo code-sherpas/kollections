626628f074cb0a78de6fd389fa288abb86f5ed2a
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2021-09-04 23:02:22 +0200

    change

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,5 +7,5 @@
  * in the user manual at https://docs.gradle.org/7.2/userguide/multi_project_builds.html
  */
 
-rootProject.name = "hackerrank"
+rootProject.name = "kotlin-data-structures"
 include("lib")

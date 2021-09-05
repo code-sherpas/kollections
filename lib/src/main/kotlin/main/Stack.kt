a0dd21a7ca6b690f65688656a7b70f8f2668ccea
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2021-09-05 18:02:05 +0200

    change

--- lib/src/main/kotlin/main/Stack.kt
+++ lib/src/main/kotlin/main/Stack.kt
@@ -3,5 +3,19 @@ package main
 /**
  * https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
  */
-class Stack {
+class Stack<E> {
+    val size: Int
+        get() = TODO("Not yet implemented")
+
+    fun push(element: E) {
+        TODO()
+    }
+
+    fun pop(): E {
+        TODO()
+    }
+
+    fun empty(): Boolean {
+        TODO()
+    }
 }

0656eb28ec4ca968f8fcfef3c9e101107719408f
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2021-09-06 19:46:42 +0200

    change

--- lib/src/main/kotlin/main/DoublyLinkedList.kt
+++ lib/src/main/kotlin/main/DoublyLinkedList.kt
@@ -1,21 +1,21 @@
 package main
 
 
-
 /**
  * https://en.wikipedia.org/wiki/Linked_list
  */
 class DoublyLinkedList<E> : MutableList<E> {
 
-    class Node<E>(private var value: E) {
-        private var previous: Node<E>? = null
-        private var next: Node<E>? = null
+    private class Node<E>(private var value: E) {
+        var previous: Node<E>? = null
+        var next: Node<E>? = null
     }
 
     private var first: Node<E>? = null
     private var last: Node<E>? = null
 
-    override val size: Int
+    override var size: Int = 0
+        private set
         get() = TODO()
 
     override fun contains(element: E): Boolean {
@@ -46,15 +46,20 @@ class DoublyLinkedList<E> : MutableList<E> {
         TODO("Not yet implemented")
     }
 
-    override fun add(element: E): Boolean {
-        when (last) {
-            null -> {
-                last = Node(element)
-                first = last
+    override fun add(element: E): Boolean = true.also {
+        Node(element).let { newNode: Node<E> ->
+            when (last) {
+                null -> {
+                    last = newNode
+                    first = last
+                }
+                else -> {
+                    last!!.next = newNode
+                    newNode.previous = last
+                    last = newNode
+                }
             }
-            else -> TODO()
         }
-        return true
     }
 
     /**
@@ -62,8 +67,20 @@ class DoublyLinkedList<E> : MutableList<E> {
      *
      * @return `true` because the list is always modified as the result of this operation.
      */
-    fun addFirst(element: E): Boolean {
-        TODO()
+    fun addFirst(element: E): Boolean = true.also {
+        Node(element).let { newNode: Node<E> ->
+            when (last) {
+                null -> {
+                    last = newNode
+                    first = last
+                }
+                else -> {
+                    newNode.next = first
+                    first!!.previous = newNode
+                    first = newNode
+                }
+            }
+        }
     }
 
     override fun add(index: Int, element: E) {

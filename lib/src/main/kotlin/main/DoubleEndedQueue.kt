d6cd73cbb695031187b9b514dab3a1231f7e15e9
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2021-09-08 20:23:21 +0200

    change

--- lib/src/main/kotlin/main/DoubleEndedQueue.kt
+++ lib/src/main/kotlin/main/DoubleEndedQueue.kt
@@ -1,27 +1,3 @@
 package main
 
-class DoubleEndedQueue<E> {
-    fun enqueueFront(element: E) {
-        TODO()
-    }
-
-    fun enqueueBack(element: E) {
-        TODO()
-    }
-
-    fun dequeueFront(): E {
-        TODO()
-    }
-
-    fun dequeueBack(): E {
-        TODO()
-    }
-
-    fun peekFront(): E {
-        TODO()
-    }
-
-    fun peekBack(): E {
-        TODO()
-    }
-}
+class DoubleEndedQueue<E>

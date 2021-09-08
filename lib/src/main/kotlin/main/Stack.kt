be28dc3f2f6c2ae74eefd69bf0690bf5fe8c362c
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2021-09-08 20:18:19 +0200

    change

--- lib/src/main/kotlin/main/Stack.kt
+++ lib/src/main/kotlin/main/Stack.kt
@@ -3,17 +3,4 @@ package main
 /**
  * https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
  */
-class Stack<E> {
-    private val dynamicArray = DynamicArray<E>()
-
-    val size: Int
-        get() = dynamicArray.size
-
-    fun push(element: E) {
-        dynamicArray.add(element)
-    }
-
-    fun pop(): E = dynamicArray.removeAt(dynamicArray.lastIndex)
-
-    fun isEmpty(): Boolean = dynamicArray.isEmpty()
-}
+class Stack<E>

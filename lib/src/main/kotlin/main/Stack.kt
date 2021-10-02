86a7125db6346faee4f47946600b32ca9f1c1881
thebongy <rishit.bansal0@gmail.com>
2021-10-02 16:21:24 +0530

    feat: Implement push(),isEmpty(),peek() and size() for Stack

--- lib/src/main/kotlin/main/Stack.kt
+++ lib/src/main/kotlin/main/Stack.kt
@@ -3,4 +3,14 @@ package main
 /**
  * https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
  */
-class Stack<E>
+class Stack<E> {
+    private val elements: MutableList<E> = mutableListOf()
+
+    fun push(element: E) = elements.add(element)
+
+    fun isEmpty() = elements.isEmpty()
+
+    fun peek() = elements.lastOrNull()
+
+    fun size() = elements.size
+}

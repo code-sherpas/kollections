697ea709ac2160724c205365dcf731c0765c7cac
bhaveesarna <71256969+bhaveesarna@users.noreply.github.com>
2021-10-02 16:13:04 +0530

    feat: Added isEmpty() for Singly Linked List (#42)
    * Added SinglyLinkedList and isEmpty() function
    
    * Implemented test case for isEmpty() for SLL

--- lib/src/main/kotlin/main/SinglyLinkedList.kt
+++ lib/src/main/kotlin/main/SinglyLinkedList.kt
@@ -5,4 +5,17 @@ package main
  *
  */
 
-class SinglyLinkedList<E>
+class SinglyLinkedList<E>{
+    //Class representing each node of the SLL       
+    inner class Node(var value: E){
+            var next: Node?= null
+    }
+    
+    //Head node of the SLL
+    var head : Node?= null
+    
+    //Checks if SLL is empty 
+    fun isEmpty(): Boolean{
+        return head == null
+    }
+}
